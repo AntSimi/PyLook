d68f329814dcad0f0ddbb85680e3f27fb3a364cc
A.delep <delepoulle.a@gmail.com>
2020-04-07 17:33:12 +0200

    Get lines with numba and divide in two class border/river and coast

--- pylook/coast_data.py
+++ pylook/coast_data.py
@@ -27,7 +27,6 @@ class GSHHSFile:
     INDEX_FIRST_SEG_IN_BIN = 'Id_of_first_segment_in_a_bin'
     NB_SEG_BY_BIN = 'N_segments_in_a_bin'
     INDEX_FIRST_PT_IN_SEG = 'Id_of_first_point_in_a_segment'
-    SEG_INFO = 'Embedded_npts_levels_exit_entry_for_a_segment'
     # int N_polygons_in_file(Dimension_of_scalar) ;
     # int N_segments_in_file(Dimension_of_scalar) ;
     # int N_points_in_file(Dimension_of_scalar) ;
@@ -57,36 +56,84 @@ class GSHHSFile:
             self.i_first_seg_bin = h.variables[self.INDEX_FIRST_SEG_IN_BIN][:]
             self.nb_seg_bin = h.variables[self.NB_SEG_BY_BIN][:]
             self.i_first_pt_seg = h.variables[self.INDEX_FIRST_PT_IN_SEG][:]
+
+
+    def lines(self, llcrnrlon=0, llcrnrlat=-90, urcrnrlon=360, urcrnrlat=90):
+        new_x, new_y = get_lines(
+            self.relative_x, self.relative_y,
+            self.nb_seg_bin, self.i_first_seg_bin, self.nb_pt_seg, self.i_first_pt_seg,
+            self.nb_bin_x, self.nb_bin_y, self.bin_size, llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat)
+        return new_x, new_y
+
+
+class CoastFile(GSHHSFile):
+
+    __slots__ = tuple()
+    SEG_INFO = 'Embedded_npts_levels_exit_entry_for_a_segment'
+
+    def load(self, filename):
+        super(CoastFile, self).load(filename)
+        with netCDF4.Dataset(filename) as h:
+            h.set_auto_maskandscale(False)
             seg_info = h.variables [self.SEG_INFO][:]
             self.nb_pt_seg = seg_info >> 9
 
-    def lines(self, llcrnrlon=0, llcrnrlat=-90, urcrnrlon=360, urcrnrlat=90):
-        i0, i1 = int(llcrnrlon // self.bin_size), int(urcrnrlon // self.bin_size)
-        j0, j1 = self.nb_bin_y - int((urcrnrlat + 90) // self.bin_size), self.nb_bin_y - int((llcrnrlat + 90) // self.bin_size)
-        new_x, new_y = list(), list()
 
-        for j in range(j0, j1):
-            for i in range(i0, i1):
-                i_box = i % self.nb_bin_x + self.nb_bin_x * j
-                
-                nb_seg = self.nb_seg_bin[i_box]
-                if nb_seg == 0:
-                    continue
-                start_seg = self.i_first_seg_bin[i_box]
-                last_seg = start_seg + nb_seg - 1
+class BorderRiverFile(GSHHSFile):
 
-                start_pt = self.i_first_pt_seg[start_seg]
-                last_pt = self.i_first_pt_seg[last_seg] + self.nb_pt_seg[last_seg]
-                sl_seg = slice(start_seg, last_seg + 1)
-                sl_pt = slice(start_pt, last_pt)
-                x = (self.relative_x[sl_pt] / 65535. + i)* self.bin_size
-                y = (self.relative_y[sl_pt] / 65535. + self.nb_bin_y - j)* self.bin_size - 90.
-                x, y = break_lines(x, y, self.i_first_pt_seg[sl_seg] - start_pt)
-                new_x.append(x)
-                new_y.append(y)
-        return numpy.concatenate(new_x), numpy.concatenate(new_y)
+    __slots__ = tuple()
+    SEG_INFO = 'N_points_for_a_segment'
 
-@numba.njit
+    def load(self, filename):
+        super(BorderRiverFile, self).load(filename)
+        with netCDF4.Dataset(filename) as h:
+            h.set_auto_maskandscale(False)
+            self.nb_pt_seg = h.variables [self.SEG_INFO][:]
+
+
+@numba.njit(cache=True)
+def get_lines(
+        relative_x, relative_y, nb_seg_bin, i_first_seg_bin, nb_pt_seg, i_first_pt_seg,
+        nb_bin_x, nb_bin_y, bin_size, llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat):
+    i0, i1 = int(llcrnrlon // bin_size), int(urcrnrlon // bin_size)
+    j0, j1 = nb_bin_y - int((urcrnrlat + 90) // bin_size), nb_bin_y - int((llcrnrlat + 90) // bin_size)
+    new_x, new_y = list(), list()
+    nb_pt = 0
+    for j in range(j0, j1):
+        for i in range(i0, i1):
+            i_box = int(i % nb_bin_x + nb_bin_x * j)
+
+            nb_seg = nb_seg_bin[i_box]
+            if nb_seg == 0:
+                continue
+            start_seg = i_first_seg_bin[i_box]
+            last_seg = start_seg + nb_seg - 1
+
+            start_pt = i_first_pt_seg[start_seg]
+            last_pt = i_first_pt_seg[last_seg] + nb_pt_seg[last_seg]
+            sl_seg = slice(start_seg, last_seg + 1)
+            sl_pt = slice(start_pt, last_pt)
+            x = (relative_x[sl_pt] / 65535. + i) * bin_size
+            y = (relative_y[sl_pt] / 65535. + nb_bin_y - j) * bin_size - 90. - bin_size
+            x, y = break_lines(x, y, i_first_pt_seg[sl_seg] - start_pt)
+            new_x.append(x)
+            new_y.append(y)
+            nb_pt += x.shape[0]
+    x = numpy.empty(nb_pt, dtype=numba.float32)
+    y = numpy.empty(nb_pt, dtype=numba.float32)
+    i = 0
+    for x_ in new_x:
+        nb = x_.shape[0]
+        x[i: i + nb] = x_
+        i += nb
+    i = 0
+    for y_ in new_y:
+        nb = y_.shape[0]
+        y[i: i + nb] = y_
+        i += nb
+    return x, y
+
+@numba.njit(cache=True)
 def break_lines(x, y, i_first_pt):
     nb_seg = i_first_pt.shape[0]
     nb_pt = x.shape[0]
