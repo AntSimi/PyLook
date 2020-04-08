49c65e2cbeb8dc5cf061e797a09026494a71a7da
A.delep <delepoulle.a@gmail.com>
2020-04-08 18:54:18 +0200

    Add requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
+
+min_version = (3, 7)
 
 setup(name='pylook',
       version='0.0',
       description='Python Distribution Utilities',
       author='Antoine Delepoulle',
       author_email='delepoulle.a@gmail.com',
-      packages=['pylook']
-     )
+      packages=['pylook'],
+      install_requires=[
+            'numpy',
+            'matplotlib',
+            'netCDF4',
+            'pyproj',
+            'numba',
+      ],
+      python_requires=f'>={'.'join(min_version)}'
+      )
