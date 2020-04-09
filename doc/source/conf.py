7c0fb5b32f2aaceca661cbbac973c09436d25c6d
A.delep <delepoulle.a@gmail.com>
2020-04-09 22:54:28 +0200

    Fix issue with RTD

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -64,3 +64,5 @@ sphinx_gallery_conf = {
      'examples_dirs': '../../examples',   # path to your example scripts
      'gallery_dirs': 'auto_examples',  # path to where to save gallery generated output
 }
+
+master_doc = 'index'
