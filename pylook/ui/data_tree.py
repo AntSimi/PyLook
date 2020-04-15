b87859a4d42214c4ca4e4c994bd0ee442a3f1ea7
A.delep <delepoulle.a@gmail.com>
2020-04-16 00:20:51 +0200

    start data context menu to display

--- pylook/ui/data_tree.py
+++ pylook/ui/data_tree.py
@@ -71,56 +71,63 @@ class DataTree(QtWidgets.QTreeWidget):
             if dataset.key in files_present:
                 continue
             elts.append(self.path_leaf(dataset))
-        self.addTopLevelItems(elts)
-        self.expandAll()
+        if len(elts):
+            self.addTopLevelItems(elts)
+            self.apply_filter()
+
+    def apply_filter(self):
+        for i in range(self.topLevelItemCount()):
+            path_leaf = self.topLevelItem(i)
+            child_hiden = 0
+            nb_child = path_leaf.childCount()
+            for j in range(nb_child):
+                var_leaf = path_leaf.child(j)
+                var_name, is_geo, is_time, is_depth = (
+                    var_leaf.data(0, 0),
+                    var_leaf.data(0, 3),
+                    var_leaf.data(0, 4),
+                    var_leaf.data(0, 5),
+                )
+                var_match = True
+                if self.states["var_regexp"]:
+                    var_match = self.states["var_regexp"](var_name) is not None
+                if (
+                    (self.states["checkbox_geo"] and not is_geo)
+                    or (self.states["checkbox_time"] and not is_time)
+                    or (self.states["checkbox_depth"] and not is_depth)
+                    or not var_match
+                ):
+                    var_leaf.setHidden(True)
+                    child_hiden += 1
+                    continue
+
+                var_leaf.setHidden(False)
+            file_match = True
+            if self.states["data_regexp"]:
+                file_match = (
+                    self.states["data_regexp"](path_leaf.data(0, 3)) is not None
+                )
+            path_leaf.setHidden(child_hiden == nb_child or not file_match)
+
+    def context_menu(self, event):
+        menu = QtWidgets.QMenu(self)
+        menu.addSeparator()
+        menu.addAction("Collapse all", self.collapseAll)
+        menu.addAction("Expand all", self.expandAll)
+        menu.exec(self.mapToGlobal(event))
 
     def update(self, event):
         sender = self.sender().objectName()
-        if sender in self.states.keys():
-            if "regexp" in sender:
-                try:
-                    self.states[sender] = re.compile(event).search
-                except:
-                    pass
-            else:
-                self.states[sender] = event != 0
-            for i in range(self.topLevelItemCount()):
-                path_leaf = self.topLevelItem(i)
-                child_hiden = 0
-                nb_child = path_leaf.childCount()
-                for j in range(nb_child):
-                    var_leaf = path_leaf.child(j)
-                    var_name, is_geo, is_time, is_depth = (
-                        var_leaf.data(0, 0),
-                        var_leaf.data(0, 3),
-                        var_leaf.data(0, 4),
-                        var_leaf.data(0, 5),
-                    )
-                    var_match = True
-                    if self.states["var_regexp"]:
-                        var_match = self.states["var_regexp"](var_name) is not None
-                    if (
-                        (self.states["checkbox_geo"] and not is_geo)
-                        or (self.states["checkbox_time"] and not is_time)
-                        or (self.states["checkbox_depth"] and not is_depth)
-                        or not var_match
-                    ):
-                        var_leaf.setHidden(True)
-                        child_hiden += 1
-                        continue
+        if "regexp" in sender:
+            try:
+                self.states[sender] = re.compile(event).search
+            except:
+                pass
+        else:
+            self.states[sender] = event != 0
+        self.apply_filter()
 
-                    var_leaf.setHidden(False)
-                file_match = True
-                if self.states["data_regexp"]:
-                    file_match = (
-                        self.states["data_regexp"](path_leaf.data(0, 3)) is not None
-                    )
-                path_leaf.setHidden(child_hiden == nb_child or not file_match)
-
-        elif sender == "open_files":
-            self.open_files()
-
-    def open_files(self):
+    def open_files(self, event):
         filenames, extension = QtWidgets.QFileDialog.getOpenFileNames(
             caption="File(s) to explore",
             initialFilter=self.compile_filter(self.data_store.known_extensions[:1]),
