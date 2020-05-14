7d064f81789f506ae97a4f3e4bae8c924429a7f9
A.delep <delepoulle.a@gmail.com>
2020-05-14 16:20:33 +0200

    forget to modify module import

--- examples/DataLook/pylook_sat.py
+++ examples/DataLook/pylook_sat.py
@@ -3,7 +3,7 @@ Plot fake sat
 =============
 """
 import shlex
-from pylook.data_look import data_look
+from pylook.appli.data_look import data_look
 
 cmd = "DataLook --dem  --method plot_plk --data fake_sat --method_options linew=0 --gallery"
 data_look(shlex.split(cmd)[1:])
